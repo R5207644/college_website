e36743abe335e2a83bdc40ca5bdd2016ca8d237e
sarvesh kumar <style.toward@gmail.com>
2023-07-21 03:16:49 +0530

    adding backend system at /contact page to save files in database files

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=120)
+    date=models.DateField()
+    email=models.EmailField(default='test@gmail.com')
+    sem=models.CharField(max_length=4,default='3rd')
+    review=models.CharField(max_length=200,default='verry good website')
--- home/views.py
+++ home/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
     
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        sem=request.POST.get('sem')
+        email=request.POST.get('email')
+        review=request.POST.get('review')
+        contact=Contact(name=name,date=datetime.today(),sem=sem,email=email,review=review)
+        contact.save()
+
     return render(request,'contact.html')
 
 def about(request):
